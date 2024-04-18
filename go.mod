82aab289aa17dadf708ce4abed89daf252d117c0
muzamil <muzamil@kali.kali>
2024-04-18 19:47:18 +0500

    Add files

---
+++ go.mod
@@ -0,0 +1,3 @@
+module go-gocd-demo
+
+go 1.21.3
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+ "fmt"
+ "net/http"
+)
+
+func main() {
+ http.HandleFunc("/", HelloServer)
+ fmt.Println("The server is listening...")
+ fmt.Println("on localhost:8080")
+ http.ListenAndServe(":8080", nil)
+}
+
+func HelloServer(writer http.ResponseWriter, request *http.Request) {
+ fmt.Fprintf(writer, "Hello, World!")
+}
